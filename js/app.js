5a4eae1b0daecbe2fa15131d143cdc30abfb1acf
kevin ventura <kevcoder@kevins-mbp.myfiosgateway.com>
2022-10-30 08:57:19 -0400

    selectors added, and can now target data and render to the UI

--- js/app.js
+++ js/app.js
@@ -38,4 +38,25 @@ const reviews = [
   },
 ];
 
-console.log(reviews)
+//Select items
+
+const img = document.getElementById('person-img');
+const author = document.getElementById('author');
+const job = document.getElementById('job');
+const info = document.getElementById('info');
+
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const randomBtn = document.querySelector('.random-btn');
+
+//set starting items
+let currentItem = 0;
+
+//Load initial item
+window.addEventListener('DOMContentLoaded', function(){
+  const item = reviews[currentItem];
+  img.src = item.img
+  author.textContent = item.name;
+  job.textContent = item.name;
+  info.textContent = item.text;
+})
